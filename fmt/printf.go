640f08e907f01fee2cc5fb0b0aa311f711542cb9
Gobin Sougrakpam <gobinsougrakpam@gmail.com>
2016-04-18 14:25:59 +0530

    Adding more printf examples

--- fmt/printf.go
+++ fmt/printf.go
@@ -12,9 +12,16 @@ func main() {
 
 	fmt.Printf("String formating: %s\n", "Like this")
 	fmt.Printf("Binary printing: %b\n", 10)
+	fmt.Printf("Character printing: %c\n", 100)
 	fmt.Printf("Printing Integer: %d\n", 100)
+	fmt.Printf("Scientific notation: %E\n", 123456789.0)
+	fmt.Printf("2nd Scientific notation: %e\n", 123456789.0)
 	fmt.Printf("Printing Float: %f\n", 1.23456789)
 	fmt.Printf("Printing Hex: %x\n", 100)
+	fmt.Printf("Quoted Strings: %q\n", "\"Quoted String\"")
+
+	fmt.Printf("|%15s|%15s|\n", "Green", "Lantern")
+	fmt.Printf("|%-6d|%-6d|\n", 100, 200)
 
 	p := paper{1, 2, 3}
 
