e8ca44b37c4a1fa0ea70227f3a00b63ba09597dd
Gobin Sougrakpam <gobinsougrakpam@gmail.com>
2016-04-19 18:59:34 +0530

    Adding daytime tcp listener

---
+++ http/tcplisten_Daytime.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", ":8080")
+	checkError(err)
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+
+		daytime := time.Now().String()
+		conn.Write([]byte(daytime))
+		conn.Close()
+	}
+}
+
+// func handleClient(conn net.Conn) {
+// 	var buf [512]byte
+// 	for {
+// 		stream, err := conn.Read(buf[0:])
+// 		if err != nil {
+// 			return
+// 		}
+// 		fmt.Println(string(buf[0:]))
+// 		_, err2 := conn
+// 	}
+// }
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Fatal Error: ", err)
+		os.Exit(1)
+	}
+}
